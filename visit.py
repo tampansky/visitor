7ee90d3908008c784a31b3ab159092a000aa7696
Tampansky-ID <42198256+tampansky@users.noreply.github.com>
2020-01-26 18:57:40 +0700

    Visitor.py

--- visit.py
+++ visit.py
@@ -1,31 +1,46 @@
-import requests,random,sys,time,os,base64,readline
+import requests,random,sys,time,os,base64,readline,curses 
 from thread import *
 os.system("clear")
 heder = requests.get("https://raw.githubusercontent.com/tampansky/visitor/master/user-age.txt").text
 heder = heder.split("\n")
-h = '\033[92m'
+h = '\033[92m' 
 p = '\033[97m'
 m = '\033[91m'
 br = '\033[94m'
-___banner___ = '''
-------------------------------------------
-[+] Bot Auto Visitor Blogger / Website
-[+] Author : Tampansky
-[+] YouTube : Tampansky ID
-[+] Github : http://github.com/tampansky
-------------------------------------------
-'''
+def mengetik(s):
+    for c in s + '':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.01)
+mengetik('''{}
+---------------------------------------------{}
+$ Auto Visitor  |        _     _         V1.0
+$ Black Coders  | /\   /(_)___| |_ ___  _ __ 
+$ Auto Visitor  | \ \ / / / __| __/ _ \|  __|
+$ Black Coders  |  \ V /| \__ \ || (_) | |   
+$ Auto Visitor  |   \_/ |_|___/\__\___/|_|  {}
+---------------------------------------------
+[{}~{}]{} Bot Auto Visitor Blogger / Website {}
+[{}~{}]{} Author : Tampansky {}
+[{}~{}]{} YouTube : Tampansky ID {}
+[{}~{}]{} Github : http://github.com/tampansky {} 
+---------------------------------------------
+'''.format(p,br,p,m,p,h,p,m,p,h,p,m,p,h,p,m,p,h,p))
+___banner___ = '''Note : Author Tidak Bertanggung jawab, atas dosa yang kalian perbuat ini, tools ini di buat hanya untuk bersenang senang saja
+---------------------------------------------'''
 print(___banner___)
-ip = raw_input("[+] Website Name : ")
+ip = raw_input("[{}+{}] Website Name : ".format(m,p))
 if "http" not in ip:
    ip = "https://{}".format(ip)
 try:
    requests.get(ip)
 except:
-   print ("Website not found or Connection Failed,aborting")
+   os.system("clear")
+
+   print ("{}[{}".format(p,m) + str(time.localtime()[3]) + "{}:{}".format(p,m) + str(time.localtime()[4]) + "{}:{}".format(p,m) + str(time.localtime()[5]) + "{}]{} Website not found or Connection Failed,aborting".format(p,m))
    sys.exit()
 
-thr = int(raw_input("Speed (normal : 50) : "))
+thr = int(raw_input("[{}+{}] Speed (normal : 10) : ".format(m,p)))
 def atk(proto):
 
     while True:
@@ -34,11 +49,11 @@ def atk(proto):
       try:
 	a = requests.get(proto,headers = {'User-Agent': rand})
 	if (str(a)) == "<Response [200]>":
-	   print ("[" + str(time.localtime()[3]) + ":" + str(time.localtime()[4]) + ":" + str(time.localtime()[5]) + "] Berhasil Mengirim Bot... ")
+	   print ("\t {}[{}".format(p,m) + str(time.localtime()[3]) + "{}:{}".format(p,m) + str(time.localtime()[4]) + "{}:{}".format(p,m) + str(time.localtime()[5]) + "{}] {}Berhasil Mengirim Bot{} ".format(p,br,p))
 	else:
-	   print ("This Website Is Protected By Cloudflare, This Attack Will Give No Effect")
+	   print ("\t {}[{}".format(p,m) + str(time.localtime()[3]) + "{}:{}".format(p,m) + str(time.localtime()[4]) + "{}:{}".format(p,m) + str(time.localtime()[5]) + "{}] {}This Website Is Protected By Cloudflare {}".format(p,m,p))
       except requests.exceptions.ConnectionError:
-	print ("No Connection Or Server Maybe Down")
+	print ("\t {}[{}".format(p,m) + str(time.localtime()[3]) + "{}:{}".format(p,m) + str(time.localtime()[4]) + "{}:{}".format(p,m) + str(time.localtime()[5]) + "{}] {}No Connection Or Server Maybe Down{}".format(p,m,p))
 
 bi = ""
 
@@ -54,8 +69,8 @@ def lol(bi):
   while True:
    for i in range(thr):
     start_new_thread(atk,(ip,))
-    print ("["+ str(time.localtime()[3]) + ":" + str(time.localtime()[4]) + ":" + str(time.localtime()[5]) +"] Thread ...")
-    time.sleep(1.5)
-   print ("Thread Already Full,Restarting")
+    print ("\t {}[{}".format(p,m) + str(time.localtime()[3]) + "{}:{}".format(p,m) + str(time.localtime()[4]) + "{}:{}".format(p,m) + str(time.localtime()[5]) +"{}] {}Thread...{}".format(p,br,p))
+    time.sleep(0.25)
+   print ("\t {}[{}".format(p,m) + str(time.localtime()[3]) + "{}:{}".format(p,m) + str(time.localtime()[4]) + "{}:{}".format(p,m) + str(time.localtime()[5]) + "{}] {}Thread Already Full{}".format(p,m,p))
    time.sleep(2)
 lol(bi)
